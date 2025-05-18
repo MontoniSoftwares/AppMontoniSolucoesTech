15c9e3d436c5368bfffb8f97867cdfc0febe1cec
Montoni Dev <robertomontoni.dev@gmail.com>
2025-05-18 14:20:31 -0300

    Atualiza HomePage.jsx com novos links de contato e e-mail

--- src/HomePage.jsx
+++ src/HomePage.jsx
@@ -313,7 +313,7 @@ function HomePage() {
                   ? `Reunião ONLINE\nLink do Google Meet: Será definido em breve.`
                   : `Reunião PRESENCIAL\nEndereço: ${addressText}`
               }\nObservações: ${observation || "Nenhuma"}`;
-              const companyMessageUrl = `https://wa.me/+5522999998352?text=${encodeURIComponent(
+              const companyMessageUrl = `https://wa.me/+552999998352?text=${encodeURIComponent(
                 companyMessage
               )}`;
               try {
@@ -647,66 +647,127 @@ function HomePage() {
           <div>
             <h2 className="text-4xl font-bold mb-6">Entre em Contato</h2>
             <p className="text-xl mb-4">
-              Telefone e Whatsapp:{" "}
+              Email:{" "}
               <a
-                href="https://wa.me/5522999998352"
-                className="text-pink-400 hover:text-pink-600 flex justify-center items-center"
-                target="_blank"
-                rel="noopener noreferrer"
+                href="mailto:robertomontoni.dev@gmail.com"
+                className="text-pink-400 hover:text-pink-600"
               >
-                <span className="mr-2">
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    width="20"
-                    height="20"
-                    viewBox="0 0 24 24"
-                    fill="currentColor"
-                    className="text-pink-400"
-                  >
-                    <path d="M12 0C5.373 0 0 5.373 0 12c0 2.134.563 4.245 1.626 6.082L0 24l5.916-1.626A11.934 11.934 0 0012 24c6.627 0 12-5.373 12-12S18.627 0 12 0zm0 22.001c-1.963 0-3.838-.567-5.44-1.63l-.389-.206-3.514.966.966-3.514-.206-.389A9.934 9.934 0 012 12c0-5.514 4.486-10 10-10s10 4.486 10 10-4.486 10-10 10zm5.618-4.737c-.31-.155-1.838-.906-2.122-1.01-.283-.104-.49-.155-.696.155-.206.31-.798.906-.982 1.113-.184.206-.367.226-.677.072-.31-.155-1.31-.482-2.498-1.536-.923-.823-1.546-1.837-1.73-2.147-.184-.31-.019-.477.138-.632.14-.138.31-.361.465-.542.155-.181.206-.31.31-.515.103-.206.052-.387-.026-.542-.077-.155-.696-1.678-.955-2.3-.252-.6-.507-.518-.696-.518h-.595c-.206 0-.54.077-.825.387-.283.31-1.082 1.062-1.082 2.59s1.108 3.002 1.262 3.208c.155.206 2.19 3.335 5.31 4.677.74.33 1.313.527 1.765.674.74.24 1.414.206 1.946.126.595-.09 1.838-.752 2.098-1.477.258-.725.258-1.34.180-1.477-.077-.138-.283-.206-.595-.361z" />
-                  </svg>
-                </span>
-                (22) 9 9999-8352
-              </a>
-              <a
-                href="https://wa.me/5522997350582"
-                className="text-pink-400 hover:text-pink-600 flex justify-center items-center"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <span className="mr-2">
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    width="20"
-                    height="20"
-                    viewBox="0 0 24 24"
-                    fill="currentColor"
-                    className="text-pink-400"
-                  >
-                    <path d="M12 0C5.373 0 0 5.373 0 12c0 2.134.563 4.245 1.626 6.082L0 24l5.916-1.626A11.934 11.934 0 0012 24c6.627 0 12-5.373 12-12S18.627 0 12 0zm0 22.001c-1.963 0-3.838-.567-5.44-1.63l-.389-.206-3.514.966.966-3.514-.206-.389A9.934 9.934 0 012 12c0-5.514 4.486-10 10-10s10 4.486 10 10-4.486 10-10 10zm5.618-4.737c-.31-.155-1.838-.906-2.122-1.01-.283-.104-.49-.155-.696.155-.206.31-.798.906-.982 1.113-.184.206-.367.226-.677.072-.31-.155-1.31-.482-2.498-1.536-.923-.823-1.546-1.837-1.73-2.147-.184-.31-.019-.477.138-.632.14-.138.31-.361.465-.542.155-.181.206-.31.31-.515.103-.206.052-.387-.026-.542-.077-.155-.696-1.678-.955-2.3-.252-.6-.507-.518-.696-.518h-.595c-.206 0-.54.077-.825.387-.283.31-1.082 1.062-1.082 2.59s1.108 3.002 1.262 3.208c.155.206 2.19 3.335 5.31 4.677.74.33 1.313.527 1.765.674.74.24 1.414.206 1.946.126.595-.09 1.838-.752 2.098-1.477.258-.725.258-1.34.180-1.477-.077-.138-.283-.206-.595-.361z" />
-                  </svg>
-                </span>
-                (22) 9 9735-0582
+                robertomontoni.dev@gmail.com
               </a>
             </p>
             <p className="text-xl mb-4">
-              Email:{" "}
+              Telefone e WhatsApp:{" "}
               <a
-                href="mailto:montoni@montonisolucoes.com"
-                className="text-pink-400 hover:text-pink-600"
-              >
-                montoni@montonisolucoes.com
-              </a>
-            </p>
-            <p className="text-xl">
-              Site:{" "}
-              <a
-                href="https://montonisolucoes.com"
-                className="text-pink-400 hover:text-pink-600"
+                href="https://wa.me/552999998352"
+                className="text-pink-400 hover:text-pink-600 flex justify-center items-center"
                 target="_blank"
                 rel="noopener noreferrer"
               >
-                montonisolucoes.com
+                <span className="mr-2">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="20"
+                    height="20"
+                    viewBox="0 0 24 24"
+                    fill="currentColor"
+                    className="text-pink-400"
+                  >
+                    <path d="M12 0C5.373 0 0 5.373 0 12c0 2.134.563 4.245 1.626 6.082L0 24l5.916-1.视0.0pt;626A11.934 11.934 0 0012 24c6.627 0 12-5.373 12-12S18.627 0 12 0zm0 22.001c-1.963 0-3.838-.567-5.44-1.63l-.389-.206-3.514.966.966-3.514-.206-.389A9.934 9.934 0 012 12c0-5.514 4.486-10 10-10s10 4.486 10 10-4.486 10-10 10zm5.618-4.737c-.31-.155-1.838-.906-2.122-1.01-.283-.104-.49-.155-.696.155-.206.31-.798.906-.982 1.113-.184.206-.367.226-.677.072-.31-.155-1.31-.482-2.498-1.536-.923-.823-1.546-1.837-1.73-2.147-.184-.31-.019-.477.138-.632.14-.138.31-.361.465-.542.155-.181.206-.31.31-.515.103-.206.052-.387-.026-.542-.077-.155-.696-1.678-.955-2.3-.252-.6-.507-.518-.696-.518h-.595c-.206 0-.54.077-.825.387-.283.31-1.082 1.062-1.082 2.59s1.108 3.002 1.262 3.208c.155.206 2.19 3.335 5.31 4.677.74.33 1.313.527 1.765.674.74.24 1.414.206 1.946.126.595-.09 1.838-.752 2.098-1.477.258-.725.258-1.34.180-1.477-.077-.138-.283-.206-.595-.361z" />
+                  </svg>
+                </span>
+                (29) 9 9998-352
+              </a>
+            </p>
+            <p className="text-xl mb-4">
+              GitHub:{" "}
+              <a
+                href="https://github.com/MontoniSoftwares"
+                className="text-pink-400 hover:text-pink-600 flex justify-center items-center"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <span className="mr-2">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="20"
+                    height="20"
+                    viewBox="0 0 24 24"
+                    fill="currentColor"
+                    className="text-pink-400"
+                  >
+                    <path d="M12 0c-6.626 0-12 5.373-12 12 0 5.302 3.438 9.8 8.207 11.387.599.111.793-.261.793-.577v-2.234c-3.338.726-4.033-1.416-4.033-1.416-.546-1.387-1.333-2.21-1.333-2.21-1.087-.744.083-.729.083-.729 1.205.084 1.838 1.237 1.838 1.237 1.071 1.834 2.809 1.305 3.495.998.108-.776.417-1.305.76-1.605-2.665-.3-5.466-1.332-5.466-5.931 0-1.311.469-2.381 1.236-3.221-.124-.303-.535-1.524.117-3.176 0 0 1.008-.322 3.301 1.23.957-.266 1.983-.399 3.003-.404 1.02.005 2.047.138 3.006.404 2.291-1.552 3.297-1.23 3.297-1.23.653 1.653.242 2.874.118 3.176.77.84 1.235 1.911 1.235 3.221 0 4.609-2.807 5.624-5.479 5.921.43.372.823 1.102.823 2.222v3.293c0 .319.192.694.801.576 4.765-1.589 8.199-6.086 8.199-11.386 0-6.627-5.373-12-12-12z" />
+                  </svg>
+                </span>
+                GitHub
+              </a>
+            </p>
+            <p className="text-xl mb-4">
+              LinkedIn:{" "}
+              <a
+                href="https://www.linkedin.com/in/robertomontoni/"
+                className="text-pink-400 hover:text-pink-600 flex justify-center items-center"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <span className="mr-2">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="20"
+                    height="20"
+                    viewBox="0 0 24 24"
+                    fill="currentColor"
+                    className="text-pink-400"
+                  >
+                    <path d="M19 0h-14c-2.761 0-5 2.239-5 5v14c0 2.761 2.239 5 5 5h14c2.762 0 5-2.239 5-5v-14c0-2.761-2.238-5-5-5zm-11 19h-3v-11h3v11zm-1.5-12.268c-.966 0-1.75-.79-1.75-1.764s.784-1.764 1.75-1.764 1.75.79 1.75 1.764-.783 1.764-1.75 1.764zm13.5 12.268h-3v-5.604c0-3.368-4-3.113-4 0v5.604h-3v-11h3v1.765c1.396-2.586 7-2.777 7 2.476v6.759z" />
+                  </svg>
+                </span>
+                LinkedIn
+              </a>
+            </p>
+            <p className="text-xl mb-4">
+              Instagram:{" "}
+              <a
+                href="https://www.instagram.com/montonisolucoestech/"
+                className="text-pink-400 hover:text-pink-600 flex justify-center items-center"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <span className="mr-2">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="20"
+                    height="20"
+                    viewBox="0 0 24 24"
+                    fill="currentColor"
+                    className="text-pink-400"
+                  >
+                    <path d="M12 2.163c3.204 0 3.584.012 4.85.07 3.252.148 4.771 1.691 4.919 4.919.058 1.265.069 1.645.069 4.849 0 3.205-.012 3.584-.069 4.849-.149 3.225-1.664 4.771-4.919 4.919-1.266.058-1.645.07-4.849.07-3.204 0-3.584-.012-4.849-.07-3.26-.149-4.771-1.699-4.919-4.92-.058-1.265-.07-1.644-.07-4.849 0-3.204.013-3.583.07-4.849.149-3.227 1.664-4.771 4.919-4.919 1.266-.057 1.645-.069 4.849-.069zm0-2.163c-3.259 0-3.667.014-4.947.072-4.358.2-6.78 2.618-6.98 6.98-.059 1.281-.073 1.689-.073 4.948 0 3.259.014 3.667.072 4.948.2 4.358 2.618 6.78 6.98 6.98 1.281.058 1.689.072 4.948.072 3.259 0 3.667-.014 4.948-.072 4.354-.2 6.782-2.618 6.982-6.98.059-1.281.073-1.689.073-4.948 0-3.259-.014-3.667-.072-4.948-.2-4.354-2.617-6.779-6.982-6.979-1.281-.059-1.69-.073-4.949-.073zm0 5.838c-3.403 0-6.162 2.759-6.162 6.162s2.759 6.163 6.162 6.163 6.162-2.759 6.162-6.163c0-3.403-2.759-6.162-6.162-6.162zm0 10.162c-2.209 0-4-1.79-4-4 0-2.209 1.791-4 4-4s4 1.791 4 4c0 2.21-1.791 4-4 4zm6.406-11.845c-.796 0-1.441.645-1.441 1.44s.645 1.44 1.441 1.44c.795 0 1.439-.645 1.439-1.44s-.644-1.44-1.439-1.44z" />
+                  </svg>
+                </span>
+                Instagram
+              </a>
+            </p>
+            <p className="text-xl mb-4">
+              App:{" "}
+              <a
+                href="https://app-montoni-solucoes-tech.vercel.app/"
+                className="text-pink-400 hover:text-pink-600 flex justify-center items-center"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <span className="mr-2">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="20"
+                    height="20"
+                    viewBox="0 0 24 24"
+                    fill="currentColor"
+                    className="text-pink-400"
+                  >
+                    <path d="M17 2H7c-1.103 0-2 .897-2 2v16c0 1.103.897 2 2 2h10c1.103 0 2-.897 2-2V4c0-1.103-.897-2-2-2zM7 20V4h10v16H7zm2-3h6v2H9v-2zm0-4h6v2H9v-2zm0-4h6v2H9V9z" />
+                  </svg>
+                </span>
+                App
               </a>
             </p>
           </div>
@@ -737,7 +798,7 @@ function HomePage() {
             />
             <input
               type="email"
-              placeholder="Email"
+              placeholder="robertomontoni.dev@gmail.com"
               value={email}
               onChange={(e) => setEmail(e.target.value)}
               className="w-full p-2 mb-4 bg-gray-700 rounded text-white"
